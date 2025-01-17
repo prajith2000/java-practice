e8adb177f29e6e936d669dd0a947943a515e88fc
prajith2000 <147729277+prajith2000@users.noreply.github.com>
2025-01-17 11:49:38 +0530

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,28 @@
+// Method overloading
+public class Calculator {
+
+    // Method to add two integers
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    // Overloaded method to add three integers
+    public int add(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    // Overloaded method to add two double values
+    public double add(double a, double b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+
+        // Calling different versions of the add method
+        System.out.println("Sum of two integers: " + calc.add(5, 10));
+        System.out.println("Sum of three integers: " + calc.add(5, 10, 15));
+        System.out.println("Sum of two doubles: " + calc.add(5.5, 10.5));
+    }
+}
+
